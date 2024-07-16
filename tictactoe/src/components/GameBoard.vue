ac7d67a12ae17aace21c9e7748dd042bc94b9cc8
lsioctl <lsioctl@gmail.com>
2024-07-16 18:19:23 -0400

    first commit

---
+++ tictactoe/src/components/GameBoard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import SquareItem from './SquareItem.vue'
+</script>
+
+<template>
+  <div class="board">
+    <div class="board_row">
+      <SquareItem />
+      <SquareItem />
+      <SquareItem />
+    </div>
+    <div class="board_row">
+      <SquareItem />
+      <SquareItem />
+      <SquareItem />
+    </div>
+    <div class="board_row">
+      <SquareItem />
+      <SquareItem />
+      <SquareItem />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50%;
+  aspect-ratio: 1/1;
+  flex-direction: column;
+}
+
+.board_row {
+  height: 33%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ tictactoe/src/components/SquareItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+function onSquareClicked() {
+  console.log('clicked')
+}
+</script>
+
+<template>
+  <div class="square" @click="onSquareClicked">X</div>
+</template>
+
+<style scoped>
+.square {
+  box-sizing: border-box;
+  border: 1px solid #000;
+  width: 33%;
+  height: 100%;
+  font-family: cursive;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
